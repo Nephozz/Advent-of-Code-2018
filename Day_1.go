bfc271400bb6bedb32318946e7addeb6f688fb83
Thomas Bocandé <thomasbocande@gmail.com>
2024-03-19 01:31:00 +0100

    day1

---
+++ Day_1.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input, err := os.ReadFile("inputs/day1")
+	if err != nil {
+		fmt.Println("Error opening file")
+		fmt.Println(err)
+		return
+	}
+
+	lines := strings.Split(string(input), "\n")
+	sum := 0
+	frequencies := make(map[int]bool)
+
+	for {
+		for _, line := range lines {
+			if line == "" {
+				continue
+			}
+			number, err := strconv.Atoi(line)
+			if err != nil {
+				fmt.Println("Error parsing number")
+				fmt.Println(err)
+				return
+			}
+
+			sum += number
+
+			if frequencies[sum] {
+				fmt.Println("First frequency reached twice: ", sum)
+				return
+			}
+
+			frequencies[sum] = true
+		}
+	}
+}
