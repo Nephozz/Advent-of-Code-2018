2865489cae82ab1bbd78e4d76f52ddf552a6f271
Thomas Bocandé <thomasbocande@gmail.com>
2024-03-19 02:31:06 +0100

    day 2 start

--- days/Day_2.go
+++ days/Day_2.go
@@ -1,5 +1,43 @@
 package days
 
-func Day2() {
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
+func Day2() {
+	input, err := os.ReadFile("inputs/day2")
+	if err != nil {
+		fmt.Println("Error, couldn't read the file!: ", err)
+		return
+	}
+
+	lines := strings.Split(string(input), "\n")
+	var twoTimes, threeTimes int
+
+	for _, line := range lines {
+		letterCounts := make(map[rune]int)
+
+		for _, letter := range line {
+			letterCounts[letter]++
+		}
+
+		twoFound, threeFound := false, false
+
+		for _, count := range letterCounts {
+			if count == 2 && !twoFound {
+				twoTimes++
+				twoFound = true
+			}
+			if count == 3 && !threeFound {
+				threeTimes++
+				threeFound = true
+			}
+		}
+	}
+
+	checksum := twoTimes * threeTimes
+
+	fmt.Println("Checksum is:", checksum)
 }
